cb0c8f9161172f30b64f64ef974f6490c1daf9a7
andrewk1230 <andrewkao1230@gmail.com>
2023-09-13 13:09:14 +0800

    Andrew Kao (TA)

--- src/main/java/com/example/VariablesAssignment.java
+++ src/main/java/com/example/VariablesAssignment.java
@@ -3,44 +3,49 @@ package com.example;
 public class VariablesAssignment {
 
     // TODO: Declare and initialize a byte variable with any value between 1 and 127
-    byte byteVariable;
+    byte byteVariable = 2;
 
     // TODO: Declare and initialize a short variable with any value between 1 and 32767
-    short shortVariable;
+    short shortVariable = 234;
 
     // TODO: Declare and initialize an int variable with any positive value
-    int intVariable;
+    int intVariable = 234;
 
     // TODO: Declare and initialize a long variable with any positive value
-    long longVariable;
+    long longVariable = 123;
 
     // TODO: Declare and initialize a float variable with any positive value
-    float floatVariable;
+    float floatVariable = 12.5f;
 
     // TODO: Declare and initialize a double variable with any positive value
-    double doubleVariable;
+    double doubleVariable = 123.23;
 
     // TODO: Declare and initialize a char variable with the initial of your first name
-    char charVariable;
+    char charVariable = 's';
 
     // TODO: Declare and initialize a boolean variable with the value true or false
-    boolean booleanVariable;
+    boolean booleanVariable = true;
 
     // TODO: Create a method that accepts two integers as parameters and returns their sum
     public int sum(int a, int b) {
         // Your code here
-        return 0; // Change this line to return the actual sum
+        
+        return a+b; // Change this line to return the actual sum
     }
 
     // TODO: Create a method that accepts two double parameters and returns their product
     public double product(double a, double b) {
-        // Your code here
-        return 0; // Change this line to return the actual product
+        // Your code her
+        return a*b; // Change this line to return the actual product
     }
 
     // TODO: Create a method that accepts a string parameter and returns it in reverse
     public String reverseString(String str) {
         // Your code here
-        return ""; // Change this line to return the reversed string
+        String word = "";
+        for(int i = str.length()-1; i>=0;i--){
+            word += str.charAt(i);
+        }
+        return word; // Change this line to return the reversed string
     }
 }
